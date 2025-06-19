a6b509f7be29aa65d521a72999d2fa4bf5ae6fa6
mohsin241 <ms0088212@gmail.com>
2025-06-19 17:16:40 +0530

    Revert "update"
    This reverts commit bb6b1a929217a90a7ec9e42d894f8a6b060cbeba.

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -13,20 +13,20 @@ const Home = () => {
   return (
     <>
       <Helmet>
-        <title>R J Enterprise | Trusted Steel Supplier in India</title>
+        <title>R J Enterprise | Premium Steel Supplier & Distributor in India</title>
         <meta name="description" content="R J Enterprise offers high-quality TMT bars, steel plates, hot rolled coils & industrial steel products. ISO 9001:2008 certified steel supplier serving construction, oil & gas, manufacturing industries." />
         <meta name="keywords" content="steel supplier, TMT bars, mild steel plates, boiler quality plates, hot rolled coils, construction steel, industrial steel" />
         
         {/* Open Graph / Facebook */}
         <meta property="og:type" content="website" />
         <meta property="og:url" content="https://www.rjenterpriseinfo.com/" />
-        <meta property="og:title" content="R J Enterprise | Trusted Steel Supplier in India" />
+        <meta property="og:title" content="R J Enterprise | Premium Steel Supplier & Distributor in India" />
         <meta property="og:description" content="Leading supplier of industrial-grade steel products including TMT bars, plates & coils for construction, manufacturing & oil & gas sectors." />
         <meta property="og:image" content="../assets/logo.png" />
         
         {/* Twitter */}
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content="R J Enterprise | Trusted Steel Supplier in India" />
+        <meta name="twitter:title" content="R J Enterprise | Premium Steel Supplier & Distributor" />
         <meta name="twitter:description" content="Premium steel products for construction, manufacturing & industrial applications. ISO 9001:2008 certified." />
         
         {/* Canonical URL */}
